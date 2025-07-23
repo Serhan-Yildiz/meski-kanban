2385d098c6bed7410b3cce216198f7a606ac6cff
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-23 10:28:27 +0300

    db.js adjusted for render deployment

--- meski-kanban-backend/db/db.js
+++ meski-kanban-backend/db/db.js
@@ -7,7 +7,9 @@ const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
