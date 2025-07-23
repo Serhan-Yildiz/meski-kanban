2e5e362b58e8c5849426fa7ef057a6627656616d
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-23 10:58:23 +0300

    frontend backend connection establishment

--- meski-kanban-frontend/src/api/axios.js
+++ meski-kanban-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // senin Node.js backend'in URL'si
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
