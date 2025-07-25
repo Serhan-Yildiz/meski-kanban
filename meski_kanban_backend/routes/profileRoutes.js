12da02effed0f0aa6d7079e543d1f4c0a5be508f
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-25 15:31:29 +0300

    dashboard page

--- meski_kanban_backend/routes/profileRoutes.js
+++ meski_kanban_backend/routes/profileRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getProfile, changePassword } from "../controllers/profileController.js";
-import auth from "../authMiddleware.js";
+import {
+  getProfile,
+  changePassword,
+} from "../controllers/profileController.js";
+import { authenticate } from "../authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/profile", auth, getProfile);
-router.put("/change-password", auth, changePassword);
+router.get("/profile", authenticate, getProfile);
+router.put("/change-password", authenticate, changePassword);
 
 export default router;
