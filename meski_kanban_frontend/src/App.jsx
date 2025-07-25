b2f7668c1a32600f0b4a55439ce516498e11f9c6
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-25 14:34:34 +0300

    purge

--- meski_kanban_frontend/src/App.jsx
+++ meski_kanban_frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
+import AuthSuccess from "./pages/AuthSuccess";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/auth/success" element={<AuthSuccess />} />
       </Routes>
     </BrowserRouter>
   );
