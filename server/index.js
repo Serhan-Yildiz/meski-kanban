0470d592b446d5931a36913e06e96e96fe6a2b72
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-28 08:45:47 +0300

    CORS

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ import boardRoutes from "./routes/boardRoutes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/auth", authRoutes);
