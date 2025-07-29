4d05bad091e8e482478ef049e1a106b140611305
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-29 11:22:10 +0300

    Added 404 page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,8 +1,17 @@
-export default function NotFoundPage() {
+import { Link } from "react-router-dom";
+import "./App.css";
+
+export default function NotFound() {
   return (
-    <div className="centered-page">
+    <div className="notfound-container">
       <h1>404</h1>
-      <p>Sayfa bulunamadı</p>
+      <p>
+        Aradığınız sayfa bulunamadı. Link yanlış olabilir veya sayfa kaldırılmış
+        olabilir.
+      </p>
+      <Link to="/">
+        <button className="back-home-button">Ana Sayfaya Dön</button>
+      </Link>
     </div>
   );
 }
