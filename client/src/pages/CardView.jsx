59238e1adff3b9f9557b254e66fa54bcced96315
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-07-29 09:19:31 +0300

    CSS

--- client/src/pages/CardView.jsx
+++ client/src/pages/CardView.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "./axios";
+import axios from "axios";
 
 export default function CardView() {
   const { id } = useParams();
