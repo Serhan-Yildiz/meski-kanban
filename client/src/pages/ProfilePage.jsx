27d62c7db66ca3f90aec56385121fbbe3f6f3445
Serhan-Yildiz <serhanyildiz33@gmail.com>
2025-08-01 14:02:37 +0300

    profile for googles

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -92,7 +92,7 @@ export default function ProfilePage() {
     navigate("/");
   };
 
-  const isGoogleUser = user?.password === "google";
+  const isGoogleUser = user?.password_hash === "google";
 
   const securityQuestions = [
     "İlk evcil hayvanınızın adı nedir?",
